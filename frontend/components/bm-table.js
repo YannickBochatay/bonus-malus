88e730798db5ef8dde7df3dbf2e6f321d743a232
Yannick Bochatay <yannick.bochatay@free.fr>
2026-02-05 23:54:13 +0100

    Correction bug d'affichage des scores

--- frontend/components/bm-table.js
+++ frontend/components/bm-table.js
@@ -77,11 +77,11 @@ class BmTable extends HTMLElement {
 
   async #update() { 
     this.querySelector(".user").textContent = this.user
-    this.querySelector(".total").textContent = this.bonus - this.malus
+    this.querySelector(".total").textContent = Number(this.bonus) + Number(this.malus)
     this.querySelector(".bonus").textContent = this.bonus
     this.querySelector(".malus").textContent = this.malus
     this.querySelector(".depenses").textContent = this.depenses
-    this.querySelector(".reste").textContent = this.bonus - this.malus - this.depenses
+    this.querySelector(".reste").textContent = Number(this.bonus) + Number(this.malus) - this.depenses
   }
 
   #setUser() {
--- frontend/sw.js
+++ frontend/sw.js
@@ -1,5 +1,5 @@
 // La version du cache
-const VERSION = "v1.1";
+const VERSION = "v1.2";
 
 // Le nom du cache
 const CACHE_NAME = `bonus-malus-${VERSION}`;
