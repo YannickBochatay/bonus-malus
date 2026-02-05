2464b53cd2c9b7582b3e0b9981b549c1791127f2
Yannick Bochatay <yannick.bochatay@free.fr>
2026-02-05 21:43:01 +0100

    changement de l'url pour la prod

--- frontend/state/utils.js
+++ frontend/state/utils.js
@@ -1,4 +1,4 @@
-export const BACKEND_BASE_URL = "http://127.0.0.1:5000/"
+export const BACKEND_BASE_URL = "https://yanb.pythonanywhere.com/"
 
 export const [,user] = /user=([^&]+)/.exec(decodeURI(location.search)) ?? []
 
