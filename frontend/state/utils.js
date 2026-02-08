462817458af763f1620d038793bc55f09298667c
Yannick Bochatay <yannick.bochatay@free.fr>
2026-02-08 08:14:50 +0100

    rétablissement de l'url de prod

--- frontend/state/utils.js
+++ frontend/state/utils.js
@@ -1,5 +1,5 @@
-// export const BACKEND_BASE_URL = "https://yanb.pythonanywhere.com/"
-export const BACKEND_BASE_URL = "http://127.0.0.1:5000/"
+export const BACKEND_BASE_URL = "https://yanb.pythonanywhere.com/"
+// export const BACKEND_BASE_URL = "http://127.0.0.1:5000/"
 
 export const PAGE_LENGTH = 20
 
