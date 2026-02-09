0b06a2ac4be41e8e42ab568989be8263f13cd27a
Yannick Bochatay <yannick.bochatay@free.fr>
2026-02-09 22:53:11 +0100

    rétablissement de la config prod

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,7 +1,7 @@
-// export const BACKEND_BASE_URL = "https://yanb.pythonanywhere.com/"
-export const BACKEND_BASE_URL = "http://127.0.0.1:5000/"
+export const BACKEND_BASE_URL = "https://yanb.pythonanywhere.com/"
+// export const BACKEND_BASE_URL = "http://127.0.0.1:5000/"
 
-export const ROOT_URL = ""
+export const ROOT_URL = "/bonus-malus/frontend"
 
 export const PAGE_LENGTH = 20
 
