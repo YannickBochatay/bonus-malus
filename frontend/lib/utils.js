c46949a327e30c9ab963e4f9a3de719c08e3efab
Yannick Bochatay <yannick.bochatay@free.fr>
2026-02-11 16:57:50 +0100

    Simplification du createState qui n'a pas besoin d'être récursif

--- frontend/lib/utils.js
+++ frontend/lib/utils.js
@@ -1,8 +1,6 @@
 import { BACKEND_BASE_URL } from "../config.js"
 export * from "../config.js"
 
-export { createState } from "./createState.js"
-
 export const [,user] = /user=([^&]+)/.exec(decodeURI(location.search)) ?? []
 
 export async function fetchJSON(url = "", options = {}) {
@@ -11,4 +9,27 @@ export async function fetchJSON(url = "", options = {}) {
   if (!res.ok) throw new Error(data.details)
   if (["POST", "PUT", "DELETE"].includes(options.method)) return data.details
   else return data
+}
+
+export function createState(initialState) {
+
+  const listeners = {};
+  
+  return {
+    state: new Proxy(initialState, {
+      set(target, prop, value) {
+        target[prop] = value;
+        listeners[prop]?.forEach(callback => callback(prop, value));
+        return true;
+      }
+    }),
+    onStateChange(prop, callback) {
+      if (!(prop in listeners)) listeners[prop] = [];
+      listeners[prop].push(callback);
+    },
+    offStateChange(prop, callback) {
+      const index = listeners[prop]?.indexOf(callback);
+      if (index != null && index !== -1) listeners[prop].splice(index, 1);
+    }
+  };
 }
